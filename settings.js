4d02811744823520f4406d046cec5de3580887eb
MDESH2 <116321722+MDESH2@users.noreply.github.com>
2023-07-08 22:23:10 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['94726662843'] //ur owner number
+global.ownernomer = "94726662843" //ur owner number2
+global.ownername = "么° 🄼 DESH 🥀 | ❤️" //ur owner name
+global.ytname = "YT: M_DESH" //ur yt chanel name
+global.socialm = "GitHub: DESH" //ur github or insta name
+global.location = "Colombo,Sri Lanka" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.botname = "MIKOTO_URABE"
+global.ownernumber = '94726662843'
+global.ownername = '么° 🄼 DESH 🥀 | ❤️'
+global.ownerNumber = ["94726662843@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@DESH"
+global.websitex = "https://youtu.be/..."
+global.wagc = "https://chat.whatsapp.com/FFCLHJGsfV9GXzuf3wj0fW"
+global.themeemoji = '👉👈'
+global.wm = "Mr DESH ツ"
+global.botscript = 'https://github.com/' //script link
+global.packname = "MIKOTO_URABE WhaatsApp Bot Create By\n\n\n\n\n\n\n\n Sticker By\n么° 🄼 DESH 🥀 | ❤️"
+global.author = "么° 🄼 DESH 🥀 | ❤️"
+global.creator = "94726662843@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
